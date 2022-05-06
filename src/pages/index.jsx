35702293e1a8da04c8ba21a4eff303fe368bd648
Thisisguillermo <guillermozaandam@hotmail.com>
2022-05-06 21:35:12 +0200

    new video upload

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <video width="100%" muted={true} autoPlay={true} loop={true} id="cadillacIntro">
+      <video width="100%" muted="true" autoplay="true" loop="true" id="cadillacIntro">
 
-        <source src="https://www.alfaromeo.com/NS_Video_Content_AR/COM/Alfa_NLE_giulia_grigia_15sec_16-9%20-%20NO%20PROMO%20WEB_3.mp4"
+        <source src="https://s1.webmshare.com/O96zW.webm"
           type="video/webm" />
         <p>Sorry, your browser doesn't support embedded videos.</p>
       </video>
