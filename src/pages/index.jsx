52995d02fd92cc2852fca88dc769cb94b17e9a1b
Thisisguillermo <guillermozaandam@hotmail.com>
2022-05-06 21:25:36 +0200

    giulia

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-// import Cadillacintro from '../components/c_intro.webm'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <video width="100%" muted="true" autoplay="true" loop="true" id="cadillacIntro">
+      <video width="100%" muted={true} autoPlay={true} loop={true} id="cadillacIntro">
 
-    <source src="https://s1.webmshare.com/GeEBb.webm"
-            type="video/webm"/>
-    Sorry, your browser doesn't support embedded videos.
-</video>
+        <source src="https://www.alfaromeo.com/NS_Video_Content_AR/COM/Alfa_NLE_giulia_grigia_15sec_16-9%20-%20NO%20PROMO%20WEB_3.mp4"
+          type="video/webm" />
+        <p>Sorry, your browser doesn't support embedded videos.</p>
+      </video>
 
     </Layout>
   )
