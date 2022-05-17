0f07066469df11c535dc0f718f47634df5f4d8eb
Thisisguillermo <guillermozaandam@hotmail.com>
2022-05-17 19:04:06 +0200

    better

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,20 +21,36 @@ const IndexPage = () => {
 const MainL = () => {
   return (
     <div>
-    <MainLayout>
-    <section className="relative">
-      <video width="100%" muted="true" autoplay="true" loop="true">
-        <source src={Intro} type="video/webm" />
-      </video>
-      <div className="absolute bg-black bg-opacity-10 inset-0 flex items-center justify-center">
-      <p className="mt-60 text-xs text-gray-300">When you approach or leave your LYRIQ, the vehicle performs an intricately choreographed lighting display across the front and rear of the vehicle. Even on the road, LYRIQ’s uniquely crafted headlamps shine like nothing else.</p>
-      </div>
-      </section>
-      <section className="relative">
-      <StaticImage src="../images/UHD_2023-LYRIQ_banner_orange.jpg" alt="Image about the new Cadillac Lyriq with an orange background" />
-      <p className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 text-xl text-white">RADIANCE THAT MOVES YOU FORWARD</p>
-      </section>
-    </MainLayout>
+      <MainLayout>
+        <section className="relative">
+          <video width="100%" muted="true" autoplay="true" loop="true">
+            <source src={Intro} type="video/webm" />
+          </video>
+          <div className="absolute bg-black bg-opacity-30 inset-0 flex items-center justify-center">
+            <p className="mt-40 sm:mt-80 text-xl font-Cadillac_Sans_A font-light text-gray-300 tracking-widest">SIMPLE AND SEAMLESS</p>
+          </div>
+        </section>
+        <section className="relative">
+          <StaticImage src="../images/UHD_2023-LYRIQ_banner_orange.jpg" alt="Image about the new Cadillac Lyriq with an orange background" />
+          <p className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 font-Cadillac_Sans_A font-light text-lg text-white">RADIANCE THAT MOVES YOU FORWARD</p>
+          <div className="absolute inset-0 flex items-center justify-center">
+            <p className="mt-40 text-xs sm:mt-80 text-xl font-Cadillac_Sans_A font-light text-white tracking-widest">A HIGHER STANDARD OF INTEGRATION</p>
+          </div>
+        </section>
+        <section className="relative bg-black text-white justify-center sm:flex">
+          <div className="relative sm:mb-16">
+            <StaticImage src="../assets/home_charging.svg" alt="uhhu" width={500} className="scale-50 sm:scale-100" />
+            <p className="absolute inset-0 flex items-center justify-center font-Cadillac_Sans_A font-light text-base text-white mt-40 sm:mt-72">190 kW ACCEPTANCE RATE (MAX CHARGING SPEED)</p>
+          </div>
+          <div className="relative sm:mb-16">
+            <StaticImage src="../assets/pub_charging.svg" alt="uhhu" width={500} className="scale-50 sm:scale-100" />
+            <p className="absolute inset-0 flex items-center justify-center font-Cadillac_Sans_A font-light text-base text-white mt-40 sm:mt-72">19.2 kW PUBLIC* OR HOME* CHARGER</p>
+          </div>
+        </section>
+        <section>
+        <StaticImage src="../images/interior.jpg" alt="uhhu" className=""/>
+        </section>
+      </MainLayout>
     </div>
   )
 }
