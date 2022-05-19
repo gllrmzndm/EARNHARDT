c668f648beafedd0e61f63f1146b866fa743b1e1
Thisisguillermo <guillermozaandam@hotmail.com>
2022-05-19 21:31:38 +0200

    huilen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,9 @@ import FooterLayout from '../components/footerlayout'
 import MainLayout from '../components/mainlayout'
 
 
+// In principe is nogal slordig, ik had gewoon alles in de Index.js kunnen doen en dan wat ik nodig had, kunnen exporteren.
+// Huilen.... maar kan snel gefixed worden in principe, maar ik laat het voor nu.
+
 
 const IndexPage = () => {
   return (
@@ -20,6 +23,8 @@ const IndexPage = () => {
 
 const MainL = () => {
   return (
+
+    // The intro video of the cadillac website.
     <div>
       <MainLayout>
         <section className="relative">
@@ -30,6 +35,8 @@ const MainL = () => {
             <p className="mt-40 sm:mt-80 text-xl font-Cadillac_Sans_A font-light text-gray-300 tracking-widest">SIMPLE AND SEAMLESS</p>
           </div>
         </section>
+
+    {/* Cadillac with orange background */}
         <section className="relative">
           <StaticImage src="../images/UHD_2023-LYRIQ_banner_orange.jpg" alt="Image about the new Cadillac Lyriq with an orange background" />
           <p className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 font-Cadillac_Sans_A font-light text-lg text-white lg:text-2xl">RADIANCE THAT MOVES YOU FORWARD</p>
@@ -37,6 +44,8 @@ const MainL = () => {
             <p className="mt-40 text-xs sm:mt-80 text-xl font-Cadillac_Sans_A font-light text-white tracking-widest lg:text-xl">A HIGHER STANDARD OF INTEGRATION</p>
           </div>
         </section>
+
+    {/* Charging details */}
         <section className="relative bg-black text-white justify-center sm:flex">
           <div className="relative sm:mb-16">
             <StaticImage src="../assets/home_charging.svg" alt="uhhu" width={500} className="scale-50 sm:scale-100" />
@@ -47,8 +56,10 @@ const MainL = () => {
             <p className="absolute inset-0 flex items-center justify-center font-Cadillac_Sans_A font-light text-base text-white mt-40 sm:mt-72">19.2 kW PUBLIC* OR HOME* CHARGER</p>
           </div>
         </section>
+
+        {/* Interior of the Cadillac */}
         <section className="relative">
-          <StaticImage src="../images/interior.jpg" alt="Image about the new Cadillac Lyriq with an orange background" />
+          <StaticImage src="../images/interior.jpg" alt="Image about the new Cadillac Lyriq interior" />
           <p className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 font-Cadillac_Sans_A font-light text-base text-white lg:text-3xl"> INTERIOR
 ENERGIZE YOUR SENSES</p>
         </section>
@@ -56,6 +67,8 @@ ENERGIZE YOUR SENSES</p>
     </div>
   )
 }
+
+// Footer of the website imported from FooterLayout.jsx
 
 const FootL = () => {
   return (
