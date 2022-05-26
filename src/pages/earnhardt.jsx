92d6dde907be52620278a497468dd3b3d566a422
Thisisguillermo <guillermozaandam@hotmail.com>
2022-05-27 00:00:26 +0200

    Mobile layout not ready yet

--- src/pages/earnhardt.jsx
+++ src/pages/earnhardt.jsx
@@ -4,22 +4,23 @@ import ReactPlayer from 'react-player'
 
 import NavLayout from '../components/navlayout'
 import FooterLayout from '../components/footerlayout'
-import MainLayout from '../components/mainlayout'
+
+
 const MainL = ({ data }) => {
   return (
-    <div className="flex flex-col">
+    <body className="flex flex-col box-border overflow-x-hidden">
 
       <div>
         <NavLayout></NavLayout>
       </div>
 
       {/* Video player */}
-      <div className="player-wrapper pointer-events-none w-screen h-screen border-4 border-orange-200">
-        <ReactPlayer className='object-fill box-border' playing='true' muted='true' loop='true' playsinline='true' width="100%"
+      <div className="player-wrapper pointer-events-none h-64 lg:pointer-events-none w-screen h-screen">
+        <ReactPlayer className='object-fill' playing='true' muted='true' loop='true' playsinline='true' width="100%"
           height="100%" url='https://youtu.be/MUU0BjJjAvk' />
       </div>
 
-      <div className="w-screen">
+      <div className="">
         <ul className="text-center bg-orange-500">
           {
             data.allFile.nodes.map(node => (
@@ -31,7 +32,7 @@ const MainL = ({ data }) => {
         </ul>
       </div>
 
-    </div >
+    </body>
   )
 }
 export const query = graphql`
